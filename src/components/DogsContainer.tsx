0e02e4005436a67815d3b681c7c2358db7c803ac
boguslawdanek <boguslawdanek@gmail.com>
2025-03-22 17:19:19 +0100

    dogs container component - WIP

---
+++ src/components/DogsContainer.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { usePets } from "@/hooks/usePetfinder";
+import { useState } from "react";
+
+const DogsContainer = () => {
+  const [filters, setFilters] = useState({
+    size: [] as string[],
+    breed: [] as string[],
+    traits: [] as string[],
+  });
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { pets } = usePets(
+    filters.size,
+    filters.breed,
+    filters.traits,
+    currentPage
+  );
+
+  return (
+    <div>
+      <h1>View</h1>
+      {pets.map((pet) => (
+        <div key={pet.id}>
+          <h2>{pet.name}</h2>
+          <p>{pet.size}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DogsContainer;
