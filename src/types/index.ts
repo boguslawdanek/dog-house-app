9f21a5a47c9e7db212d158b417b78ec52fbd7485
boguslawdanek <boguslawdanek@gmail.com>
2025-03-22 17:18:29 +0100

    added aditional types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,24 @@
+export interface Pet {
+  id: string;
+  name: string;
+  type: string;
+  size: "Small" | "Medium" | "Large" | "Extra Large";
+  gender: "Male" | "Female" | "Unknown";
+  age: "Baby" | "Young" | "Adult" | "Senior";
+}
+
 export interface PetToken {
   token_type: string;
   expires_in: number;
   access_token: string;
   expiresAt: number;
+}
+export interface PetfinderResponse {
+  animals: Pet[];
+  pagination: {
+    count_per_page: number;
+    total_count: number;
+    current_page: number;
+    total_pages: number;
+  };
 }
