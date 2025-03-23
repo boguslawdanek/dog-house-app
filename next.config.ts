af147b6b297c5a53254fcdab4f3c991c63f11f7b
boguslawdanek <boguslawdanek@gmail.com>
2025-03-23 09:53:24 +0100

    next.config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["dl5zpyw5k3jeb.cloudfront.net", "dbw3zep4prcju.cloudfront.net"],
+  },
 };
 
 export default nextConfig;
