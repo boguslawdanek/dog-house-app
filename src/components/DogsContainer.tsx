fb9de70ae8a4125a3fb1f954aa9df8b470fdb1c0
boguslawdanek <boguslawdanek@gmail.com>
2025-03-23 12:08:24 +0100

    DogContainer updated

--- src/components/DogsContainer.tsx
+++ src/components/DogsContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { usePets } from "@/hooks/usePetfinder";
+import { usePets, usePetfinder } from "@/hooks/usePetfinder";
 import { useState } from "react";
 import DogCard from "./DogCard";
 import LoadingSpinner from "./LoadingSpinner";
@@ -12,16 +12,31 @@ const DogsContainer = () => {
   });
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [showFavorites, setShowFavorites] = useState(false);
 
-  const { pets, isLoading } = usePets(
+  const { pets, isLoading, pagination } = usePets(
     filters.size,
     filters.breed,
     filters.traits,
     currentPage
   );
 
+  const { toggleLikePet, isPetLiked, likedPets } = usePetfinder();
+
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto">
+      <div className="mb-6 flex flex-col sm:flex-row sm:items-center justify-between gap-2">
+        <h2 className="text-xl font-medium">
+          {showFavorites ? "Your Favorite Dogs" : "Dogs for Adoption"}
+        </h2>
+
+        {!isLoading && (
+          <p className="text-sm text-muted-foreground">
+            {pagination?.total_count}
+          </p>
+        )}
+      </div>
+
       {isLoading && (
         <div className="flex flex-col items-center justify-center py-20">
           <LoadingSpinner size="large" />
@@ -33,7 +48,12 @@ const DogsContainer = () => {
       {!isLoading && pets.length > 0 && (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {pets.map((pet) => (
-            <DogCard key={pet.id} pet={pet} />
+            <DogCard
+              key={pet.id}
+              pet={pet}
+              onToggleLike={toggleLikePet}
+              isLiked={isPetLiked(pet.id)}
+            />
           ))}
         </div>
       )}
