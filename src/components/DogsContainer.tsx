d2763a17395e6aa3d3a7fad1c0580d310a2fe0ff
boguslawdanek <boguslawdanek@gmail.com>
2025-03-23 13:21:24 +0100

    Container component updated

--- src/components/DogsContainer.tsx
+++ src/components/DogsContainer.tsx
@@ -3,8 +3,12 @@ import { usePets, usePetfinder } from "@/hooks/usePetfinder";
 import { useState } from "react";
 import DogCard from "./DogCard";
 import LoadingSpinner from "./LoadingSpinner";
+import Pagination from "./Pagination";
+import EmptyLikedPetsList from "./EmptyLikedPetsList";
+import Filters from "./Filters";
 
 const DogsContainer = () => {
+  const { toggleLikePet, isPetLiked, likedPets } = usePetfinder();
   const [filters, setFilters] = useState({
     size: [] as string[],
     breed: [] as string[],
@@ -21,43 +25,77 @@ const DogsContainer = () => {
     currentPage
   );
 
-  const { toggleLikePet, isPetLiked, likedPets } = usePetfinder();
+  const displayedPets = showFavorites
+    ? pets.filter((pet) => isPetLiked(pet.id))
+    : pets;
+
+  const toggleFavoritePets = () => {
+    setShowFavorites((prev) => !prev);
+  };
 
   return (
-    <div className="mx-auto">
-      <div className="mb-6 flex flex-col sm:flex-row sm:items-center justify-between gap-2">
-        <h2 className="text-xl font-medium">
-          {showFavorites ? "Your Favorite Dogs" : "Dogs for Adoption"}
-        </h2>
+    <>
+      <div className="mx-auto">
+        <div className="max-w-7xl mx-auto">
+          <Filters />
+          <div className="mb-6 flex flex-col sm:flex-row sm:items-center justify-between gap-2">
+            <h2 className="text-xl font-medium">
+              {showFavorites ? "Your Favorite Dogs" : "Dogs for Adoption"}
+            </h2>
+            {!isLoading && (
+              <p className="text-sm">
+                {showFavorites
+                  ? `${displayedPets.length} liked ${
+                      displayedPets.length === 1 ? "dog" : "dogs"
+                    }`
+                  : pagination?.total_count
+                  ? `Showing ${displayedPets.length} of ${
+                      pagination.total_count
+                    } available ${
+                      pagination.total_count === 1 ? "dog" : "dogs"
+                    }`
+                  : "No dogs found matching your criteria"}
+              </p>
+            )}
+          </div>
 
-        {!isLoading && (
-          <p className="text-sm text-muted-foreground">
-            {pagination?.total_count}
-          </p>
-        )}
+          {isLoading && (
+            <div className="flex flex-col items-center justify-center py-20">
+              <LoadingSpinner size="large" />
+              <p className="animate-pulse-light">Loading dogs...</p>
+            </div>
+          )}
+
+          {!isLoading && pets.length > 0 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {displayedPets.map((pet) => (
+                <DogCard
+                  key={pet.id}
+                  pet={pet}
+                  onToggleLike={toggleLikePet}
+                  isLiked={isPetLiked(pet.id)}
+                />
+              ))}
+            </div>
+          )}
+
+          <EmptyLikedPetsList
+            isLoading={isLoading}
+            displayedPets={displayedPets}
+            showFavorites={showFavorites}
+            toggleFavoritesView={toggleFavoritePets}
+          />
+
+          <Pagination
+            isLoading={isLoading}
+            showFavorites={showFavorites}
+            pagination={pagination || null}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </div>
       </div>
-
-      {isLoading && (
-        <div className="flex flex-col items-center justify-center py-20">
-          <LoadingSpinner size="large" />
-          <p className="text-muted-foreground animate-pulse-light">
-            Loading dogs...
-          </p>
-        </div>
-      )}
-      {!isLoading && pets.length > 0 && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {pets.map((pet) => (
-            <DogCard
-              key={pet.id}
-              pet={pet}
-              onToggleLike={toggleLikePet}
-              isLiked={isPetLiked(pet.id)}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
