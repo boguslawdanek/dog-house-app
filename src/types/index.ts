ae866a64e62a53eb914d7ae810f6ccf646ed58e3
boguslawdanek <boguslawdanek@gmail.com>
2025-03-23 09:54:34 +0100

    added correct Pet Type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,55 @@ export interface Pet {
   id: string;
   name: string;
   type: string;
+  breeds: {
+    primary: string;
+    secondary?: string;
+    mixed: boolean;
+    unknown: boolean;
+  };
   size: "Small" | "Medium" | "Large" | "Extra Large";
   gender: "Male" | "Female" | "Unknown";
   age: "Baby" | "Young" | "Adult" | "Senior";
+  colors: {
+    primary?: string;
+    secondary?: string;
+    tertiary?: string;
+  };
+  photos: {
+    small: string;
+    medium: string;
+    large: string;
+    full: string;
+  }[];
+  status: "adoptable" | "adopted" | "found";
+  attributes: {
+    spayed_neutered: boolean;
+    house_trained: boolean;
+    declawed?: boolean;
+    special_needs: boolean;
+    shots_current: boolean;
+  };
+  environment: {
+    children?: boolean;
+    dogs?: boolean;
+    cats?: boolean;
+  };
+  tags: string[];
+  description: string;
+  organization_id: string;
+  contact: {
+    email?: string;
+    phone?: string;
+    address: {
+      address1?: string;
+      address2?: string;
+      city: string;
+      state: string;
+      postcode: string;
+      country: string;
+    };
+  };
+  published_at: string;
 }
 
 export interface PetToken {
